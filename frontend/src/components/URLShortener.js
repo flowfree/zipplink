b06ca15f5e1f3be975339d1144e96b136d069c00
Nash <nashruddin.amin@gmail.com>
2021-03-19 21:48:29 +0700

    added bootstrap

--- frontend/src/components/URLShortener.js
+++ frontend/src/components/URLShortener.js
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+
 export default function URLShortener() {
+  const [url, setUrl] = useState('https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-networking.html')
+
+  function handleShortenUrl(e) {
+    e.preventDefault()
+    setUrl('https://zipp.link/ax1eb')
+  }
+
   return (
     <div>
-      <h1>Shorten Your Link</h1>
+      <h1>Shorten Your URL</h1>
+      <form action="" className="form" onSubmit={handleShortenUrl}>
+        <div className="input-group mt-3">
+          <input 
+            type="text" 
+            name="url" 
+            value={url}
+            onChange={e => setUrl(e.target.value)}
+            className="form-control form-control-lg"
+            placeholder="Paste your long URL"
+          />
+          <button 
+            className="btn btn-primary"
+            onClick={handleShortenUrl}
+          >Shorten URL</button>
+        </div>
+      </form>
     </div>
   )
 }
