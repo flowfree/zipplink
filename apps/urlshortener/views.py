175118792545f9477e45406377b3613c6c130147
Nash <nashruddin.amin@gmail.com>
2021-03-29 07:57:20 +0700

    Allow only 3 requests/min

--- apps/urlshortener/views.py
+++ apps/urlshortener/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import redirect, get_object_or_404
 from rest_framework import viewsets
+from rest_framework.throttling import AnonRateThrottle
 from .models import URL
 from .serializers import URLSerializer
+
+
+class BasicRateThrottle(AnonRateThrottle):
+    rate = '3/min'
 
 
 class URLViewSet(viewsets.ModelViewSet):
     queryset = URL.objects.all()
     serializer_class = URLSerializer
+    throttle_classes = [BasicRateThrottle]
 
 
 def redirect_short_url(request, slug):
