ee6deaf281540972f30d42a0583b06729a4ebe86
Nashruddin Amin <nashruddin.amin@gmail.com>
2022-04-13 13:34:50 +0700

    Added id field

--- apps/urlshortener/models.py
+++ apps/urlshortener/models.py
@@ -4,6 +4,7 @@ from .helpers import Base62
 
 
 class URL(models.Model):
+    id = models.AutoField(primary_key=True)
     long_url = models.URLField()
     slug = models.CharField(max_length=32, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
